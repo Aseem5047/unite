0c2439f6816d0c530f9ede211d9b980c7102f845
Aseem5047 <aseemgupta2002@gmail.com>
2024-05-18 12:42:11 +0530

    profile section updated

--- app/(root)/(home)/profile/[userId]/page.tsx
+++ app/(root)/(home)/profile/[userId]/page.tsx
@@ -25,23 +25,24 @@ const UserProfilePage = () => {
 
 	const [userData, setUserData] = useState(initialState);
 	const [editData, setEditData] = useState(false);
+	const [showFullDesc, setShowFullDesc] = useState(false);
 
 	// console.log(userData);
 
 	return (
 		<div className="flex justify-start items-center size-full flex-col gap-7">
 			{/* Profile Info */}
-			<div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] items-center w-full gap-4  lg:w-1/2">
+			<div className="grid grid-cols-1 xl:grid-cols-[1fr_2fr] items-center w-full gap-10 xl:gap-4 xl:max-w-[69%]">
 				{/* user profile picture */}
 				<Image
 					src={userData.photo}
 					alt="profile picture"
 					width={24}
 					height={24}
-					className="rounded-full w-40 mx-auto"
+					className="rounded-full min-w-40 mx-auto"
 				/>
 
-				<div className="flex flex-col items-center md:items-start justify-center gap-2">
+				<div className="flex flex-col items-center lg:items-start justify-center gap-2">
 					{/* userDetails */}
 					<div className="grid grid-cols-[2fr_1fr] items-center w-full">
 						<div className="flex flex-col items-start justify-center">
@@ -63,15 +64,15 @@ const UserProfilePage = () => {
 
 					{/* user bio */}
 					<p
-						className="font-semibold pt-4 cursor-pointer hover:text-blue-1 w-full max-w-[500px] max-h-[100px] overflow-scroll no-scrollbar text-center md:text-start"
-						onClick={() => setEditData((prev) => !prev)}
+						className={`font-semibold pt-4 cursor-pointer hover:text-blue-1 w-full  no-scrollbar text-start ${
+							showFullDesc
+								? "overflow-scroll max-h-[150px] pb-4"
+								: "text-ellipsis overflow-hidden  whitespace-nowrap max-w-[600px]"
+						}`}
+						onClick={() => setShowFullDesc((prev) => !prev)}
 					>
 						{userData?.bio?.length === 0
-							? `${
-									editData
-										? "Close the Edit Description Section"
-										: "Tap to add Description or Edit the Details"
-							  }`
+							? "Add Description for your Profile and Account"
 							: userData?.bio}
 					</p>
 				</div>
